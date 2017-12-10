1acdbe69bb8472b898ae9f2347c99d2327ddccb9
Evan Watterud <cod.videos@hotmail.com>
2017-12-10 17:00:19 -0500

    chromedriver working

--- test/seleniumTests.js
+++ test/seleniumTests.js
@@ -1,2 +1,11 @@
-var assert = require("assert");
-var webdriver = require("selenium-webdriver");
+var assert = require('assert');
+var selenium = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
